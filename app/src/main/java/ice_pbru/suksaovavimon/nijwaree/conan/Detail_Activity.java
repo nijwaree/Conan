90af439db831a81533685c860d22ac1b327375f3
nijwaree <fonaugust@gmail.com>
2016-04-11 17:46:45 +0700

    step 6 finist list

--- app/src/main/java/ice_pbru/suksaovavimon/nijwaree/conan/Detail_Activity.java
+++ app/src/main/java/ice_pbru/suksaovavimon/nijwaree/conan/Detail_Activity.java
@@ -2,12 +2,47 @@ package ice_pbru.suksaovavimon.nijwaree.conan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Detail_Activity extends AppCompatActivity {
+
+    private TextView titleTextView, detailTextView;
+    private ImageView conanImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_);
+
+        // bind widget
+        bindWidget();
+
+        // Show view
+        showView();
+
+
     }
-}
+
+    private void showView() {
+        String strTitle = getIntent().getStringExtra("Title");
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.cudo);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+
+        titleTextView.setText(strTitle);
+        conanImageView.setImageResource(intIcon);
+
+        String[] srtDetail = getResources().getStringArray(R.array.conan_detail);
+        detailTextView.setText(srtDetail[intIndex]);
+
+
+    }// show view
+
+    private void bindWidget() {
+
+        titleTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        conanImageView = (ImageView) findViewById(R.id.imageView2);
+
+    }// bind widget
+}// main class
--- app/src/main/java/ice_pbru/suksaovavimon/nijwaree/conan/MainActivity.java
+++ app/src/main/java/ice_pbru/suksaovavimon/nijwaree/conan/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -65,6 +66,21 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter myAdapter = new MyAdapter(MainActivity.this, intIcon, titleString, detailStrings);
         conanListView.setAdapter(myAdapter);
 
+        conanListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Intent intent = new Intent(MainActivity.this, Detail_Activity.class);
+
+                intent.putExtra("Title", titleString);
+                intent.putExtra("Image", intIcon[i]);
+                intent.putExtra("Index", i);
+
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 
     private void buttoncon() {
@@ -73,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse("https://fonaugust.wordpress.com/2014/05/21/"));
+
 
                 startActivity(intent);
 
